13e83f59f647f42fc18b4e9a80abc9bb09bdf0b6
Sergio V <vucket07@gmail.com>
2022-12-25 20:23:13 +0100

    Feat: Adding home page and SemanticUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "semantic-ui-css/semantic.min.css";
+import "../styles/globals.css";
+import { Inter } from "@next/font/google";
+import type { AppProps } from "next/app";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,3 +1,4 @@
+import React, { FunctionComponent } from "react";
 import { GetServerSideProps } from "next";
 import Image from "next/image";
 
@@ -6,10 +7,10 @@ interface PokemonProps {
   pokemonSprites: string;
 }
 
-export const getServerSideProps: GetServerSideProps<PokemonProps> = async ({
-  params,
-}) => {
-  const pokemon = params?.id;
+export const getServerSideProps: GetServerSideProps<PokemonProps> = async (
+  context
+) => {
+  const pokemon = context.params?.id;
   const pokemonReq = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokemon}`
   );
@@ -27,7 +28,7 @@ export const getServerSideProps: GetServerSideProps<PokemonProps> = async ({
   };
 };
 
-export default function Pokemon({ pokemonData, pokemonSprites }) {
+export default function Pokemon({ pokemonData, pokemonSprites }: PokemonProps) {
   return (
     <div>
       {pokemonData.name}
