cd99a122d1d76cfef1313a03231a508fd1bc3aa1
Sergio V <vucket07@gmail.com>
2022-12-25 14:37:55 +0100

    Initial commit

---
+++ pages/pokemon/[id].tsx
@@ -0,0 +1,44 @@
+import { GetServerSideProps } from "next";
+import Image from "next/image";
+
+interface PokemonProps {
+  pokemonData: any;
+  pokemonSprites: string;
+}
+
+export const getServerSideProps: GetServerSideProps<PokemonProps> = async ({
+  params,
+}) => {
+  const pokemon = params?.id;
+  const pokemonReq = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+  );
+  const pokemonRes = await pokemonReq.json();
+
+  const spriteUrl: string = pokemonRes.sprites.front_default;
+
+  const props: PokemonProps = {
+    pokemonData: pokemonRes,
+    pokemonSprites: spriteUrl,
+  };
+
+  return {
+    props,
+  };
+};
+
+export default function Pokemon({ pokemonData, pokemonSprites }) {
+  return (
+    <div>
+      {pokemonData.name}
+      <div>
+        <Image
+          src={pokemonSprites}
+          alt={pokemonData.name}
+          width={300}
+          height={300}
+        ></Image>
+      </div>
+    </div>
+  );
+}
